cf2174c33d68c2f41c3f5f644f400dddae4fb4b2
sripwoud <me@sripwoud.xyz>
2025-05-21 16:05:40 +0200

    chore: remove jsdoc comment

--- pkgs/xoswap/src/http.ts
+++ pkgs/xoswap/src/http.ts
@@ -2,12 +2,6 @@ import { Option, type Some } from '@hazae41/option'
 import { Err, Ok, type Result } from '@hazae41/result'
 import type { ErrResponse } from './types'
 
-/**
- * @param url
- * @param method
- * @param options
- * @returns
- */
 export async function fetchResult<T>(
   url: string,
   method: 'GET' | 'POST' | 'PATCH',
